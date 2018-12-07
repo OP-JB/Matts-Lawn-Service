4864934ab9acae88c9f49255df57d246dde2308c
James Byrd <James.Byrd1265@gmail.com>
2018-12-07 18:12:10 -0500

    feat: navbar

---
+++ client/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './navbar'
+
+const Main = () => {
+  return (
+    <div id='main'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Main
---
+++ client/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div id='navbar' className="top-header">
+      <div>
+        <Link id="logo" className="flex" to="/">
+          <h2 id="logo-text">Matt's Lawn Service</h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
