31929cbe0ad61cb519e15fdcc7d4de81ce5cfb0d
James Byrd <James.Byrd1265@gmail.com>
2018-12-09 17:43:56 -0500

    style: slogan

--- client/components/home-image.js
+++ client/components/home-image.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 var HomeImage = function() {
   return (
-    <div id='home-image-container'>
-      <img id='home-image' src='https://static1.squarespace.com/static/590887ec86e6c0132cec0478/5908ab101b631bee5f6939ec/5968c86ac534a525bfe4627c/1516215361473/the-lawn-lady-polk-county-lawncare-florida-landscaping-3.jpg?format=1500w'></img>
+    <div id='home-image'>
+      <h2 id='slogan'>Professional Quality From Start To Finish</h2>
     </div>
   )
 }
 
 export default HomeImage
+
+//https://static1.squarespace.com/static/590887ec86e6c0132cec0478/5908ab101b631bee5f6939ec/5968c86ac534a525bfe4627c/1516215361473/the-lawn-lady-polk-county-lawncare-florida-landscaping-3.jpg?format=1500w
