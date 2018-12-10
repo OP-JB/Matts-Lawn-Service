d0bf0e7bd9f8b67a0e088906af9179bd046f601b
James Byrd <James.Byrd1265@gmail.com>
2018-12-10 16:26:07 -0500

    feat: service descriptions

--- client/components/service.js
+++ client/components/service.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 var Service = function(props) {
-  console.log('PROPS:  ', props)
+  var {title, photo, description} = props
   return(
     <div className='service'>
       <div className='service-container'>
         <h2 className='service-title'>{props.title}</h2>
         <div className='service-content'>
           <img className='service-image' src={props.photo}></img>
-          <p className='service-text'></p>
+          <ul className='service-text'>
+            {description.map(function(bullet){
+              return (<li key={bullet}>{bullet}</li>)
+            })}
+          </ul>
         </div>
       </div>
     </div>
