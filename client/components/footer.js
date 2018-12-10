ac1a5b00b5a53727cdba6308e2db9cd6930b3d33
James Byrd <James.Byrd1265@gmail.com>
2018-12-10 02:02:03 -0500

    feat: social-media

--- client/components/footer.js
+++ client/components/footer.js
@@ -5,6 +5,21 @@ var Footer = function() {
   return (
     <div id='footer' className="top-header">
       <div id='footer-content'>
+        <div id='footer-contact'>
+          <h3 id='footer-phone' className='footer-contact-text'>386-290-7950</h3>
+          <h4 id='footer-email'className='footer-contact-text'>lawnservicebymatt@gmail.com</h4>  
+        </div>
+        <div id='copyright'>
+          <h5 id='copyright-text'>© MATTSLAWNSERVICE</h5>
+        </div>
+        <div id='social-media'>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.facebook.com' className='social-media-icons'  >
+            <img id='facebook-icon' src='./assets/facebook-icon.png'></img>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com' className='social-media-icons'  >
+            <img id='instagram-icon' src='./assets/instagram-icon.png'></img>
+          </a>
+        </div>
       </div>
     </div>
   )
