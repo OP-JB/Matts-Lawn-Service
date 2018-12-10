f9ec2163f1f50ac32c34fa65139b93fdaf400034
James Byrd <James.Byrd1265@gmail.com>
2018-12-10 13:05:55 -0500

    feat: services render individually

--- client/components/service.js
+++ client/components/service.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-var Service =function(props) {
-    console.log(props)
+var Service = function(props) {
+    console.log('PROPS:  ', props)
     return(
-        <div id='service'>
-          <div id='service-container'>
-            <h2 id='service-title'>service Matt</h2>
-            <div id='service-content'>
-              <img id='service-image' src='./assets/matthat600.png'></img>
-              <p id='service-text'></p>
+        <div className='service'>
+          <div className='service-container'>
+            <h2 className='service-title'>{props.title}</h2>
+            <div className='service-content'>
+              <img className='service-image' src={props.photo}></img>
+              <p className='service-text'></p>
             </div>
           </div>
         </div>
