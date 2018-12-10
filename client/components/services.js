736f8e91d3f531288c144f411f318aa9da054e42
pspag85 <pspag85@gmail.com>
2018-12-10 11:06:32 -0500

    style: services header

--- client/components/services.js
+++ client/components/services.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 var Services = function() {
   return (
-    <div>Hello guy</div>
+    <div id='services'>
+        <h2 id='services-header'>Commercial & Residential Services:</h2>
+
+    </div>
   )
 }
 
