96169e0425f177e5030249847f3e2f99bf352a64
James Byrd <James.Byrd1265@gmail.com>
2018-12-10 20:47:36 -0500

    fix: contact name collision

--- client/components/contact.js
+++ client/components/contact.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
 var Contact = function() {
   return (
-    <div>Hello guy contact</div>
+    <div id='contact'>
+        <h2 id='contact-header'>
+            Contact Matt</h2>
+        <div id='contact-content'>
+            <form method='post' encType='text/plain' action='mailto:lawnservicebymatt@gContact Matt
+mail.com' id='contact-form'>
+                <label htmlFor='name' id='contact-name'></label>
+                <input name='name' type='text'/>
+                <label htmlFor='email' id='contact-email'> </label>
+                <input name='email' type='text'/>
+                <label htmlFor='subject' id='contact-subject'></label>
+                <input name='subject' type='text'/>
+                <label htmlFor='message' id='contact-message'></label>
+                <input name='message' type='text'/>
+                <button type='submit' id='contact-submit'>Submit</button>
+            </form>
+            <div id='contact-location'>
+                <div id='contact-address'>
+                </div>
+                <div id='contact-phone'>
+                </div>
+                <div id='contact-map'>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
-
 export default Contact
--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './main'
-import history from './history'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
