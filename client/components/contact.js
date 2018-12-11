880816a7a8deeaa375cfb0f9462c170d8f6f3147
pspag85 <pspag85@gmail.com>
2018-12-10 21:59:36 -0500

    style: contact inputs

--- client/components/contact.js
+++ client/components/contact.js
@@ -6,14 +6,19 @@ var Contact = function() {
             Contact Matt</h2>
         <div id='contact-content'>
             <form method='post' encType='text/plain' action='mailto:lawnservicebymatt@gmail.com' id='contact-form'>
-                <label htmlFor='name' id='contact-name'></label>
-                <input name='name' type='text'/>
-                <label htmlFor='email' id='contact-email'> </label>
-                <input name='email' type='text'/>
-                <label htmlFor='subject' id='contact-subject'></label>
-                <input name='subject' type='text'/>
-                <label htmlFor='message' id='contact-message'></label>
-                <input name='message' type='text'/>
+                <label htmlFor='name' className='contact-label' id='contact-name'>Name:</label>
+                <br/>
+                <input name='name' className='contact-input' type='text'/>
+                <label htmlFor='email' className='contact-label' id='contact-email'>Email:</label>
+                <br/>
+                <input name='email' className='contact-input' type='text'/>
+                <label htmlFor='subject' className='contact-label' id='contact-subject'>Subject:</label>
+                <br/>
+                <input name='subject' className='contact-input' type='text'/>
+                <label htmlFor='message' className='contact-label' id='contact-message'>Message:</label>
+                <br/>
+                <textarea name='message' className='contact-input' type='text' > </textarea>
+                <br/>
                 <button type='submit' id='contact-submit'>Submit</button>
             </form>
             <div id='contact-location'>
